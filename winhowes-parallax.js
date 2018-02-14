83911e756d98591bcdc67777c2512fa76f67d647
Raymond <musicsmithnz@gmail.com>
2018-02-14 18:01:31 +1300

    tidy

--- winhowes-parallax.js
+++ winhowes-parallax.js
@@ -8,14 +8,11 @@ export class WinhowesParallax extends PolymerElement {
 	static get template() {
     return `
         <!--STYLES-->
-			
-<link href="//cdn.muicss.com/mui-0.9.35/css/mui.min.css" rel="stylesheet" type="text/css" />
-<script src="//cdn.muicss.com/mui-0.9.35/js/mui.min.js"></script>
-
+            <link href="//cdn.muicss.com/mui-0.9.35/css/mui.min.css" rel="stylesheet" type="text/css" />
+            <script src="//cdn.muicss.com/mui-0.9.35/js/mui.min.js"></script>
         <!--END-STYLES-->
 
         <!--TEMPLATE-->
-			
         <!--END-TEMPLATE-->
 
         <!--SCRIPTS-->
